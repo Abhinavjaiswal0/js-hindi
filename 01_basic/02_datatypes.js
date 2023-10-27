e670ea35e1cd3987596dd81b69cc451a0ba5fa26
Abhinav <123149559+Abhinavjaiswal0@users.noreply.github.com>
2023-10-27 11:02:33 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello")  // we are using node JS not browser
+
+console.log(3 + 3)  // code readability should be high
+
+console.log("abhinav")
+
+// number => 2 to power 53
+// bigint 
+// String => ""
+// boolean => true/false
+// null => 
+// undefined =>
+// symbol => unique
+
+// object =>
+
+console.log(typeof null);  //object
+console.log(typeof undefined);  // undefined
+
